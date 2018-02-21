c303e8799b5d682fb9b1e4517283597148ceb8a4
Waleed Masoud <waleed@moqawalat.com>
2018-02-21 06:10:57 -0800

    Add AjaxToolKit Add Textbox accept only numbers

--- CSwebsite/Default.aspx
+++ CSwebsite/Default.aspx
@@ -1,5 +1,6 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CSwebsite.Default" %>
 
+
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -9,13 +10,17 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
             <asp:Label ID="Label1" runat="server" Text="Label" Font-Size="XX-Large">Test Github Repository</asp:Label>
             <br />
             <asp:Label ID="Label2" runat="server" Text="Label" Font-Size="XX-Large">This Label from visual studio</asp:Label>
             <br />
             <asp:Label ID="Label3" runat="server" Text="Label" Font-Size="XX-Large">This Label from Github</asp:Label>
             <br />
+            <asp:Label ID="Label4" runat="server" Text="Label" Font-Size="Medium">You can type numbers only</asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
             
+            <ajaxToolKit:FilteredTextBoxExtender ID="filter" runat="server" FilterType="Numbers" TargetControlID="TextBox1" />
         </div>
     </form>
 </body>
--- CSwebsite/Default.aspx.designer.cs
+++ CSwebsite/Default.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace CSwebsite {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// ScriptManager1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
+        
+        /// <summary>
         /// Label1 control.
         /// </summary>
         /// <remarks>
@@ -47,5 +56,32 @@ namespace CSwebsite {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// Label4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+        
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// filter control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::AjaxControlToolkit.FilteredTextBoxExtender filter;
     }
 }
